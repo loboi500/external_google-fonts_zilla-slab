f2c456c81a6e133f56fbd2218fa704b4a31904b2
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 17:07:30 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- fonts.mk
+++ fonts.mk
@@ -1,23 +1,3 @@
-# Copyright (C) 2018 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
-# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
-# get installed too.
-
-PRODUCT_PACKAGES := \
-    ZillaSlab-Medium.ttf \
-    ZillaSlab-MediumItalic.ttf \
-    ZillaSlab-SemiBold.ttf \
-    ZillaSlab-SemiBoldItalic.ttf
+version https://git-lfs.github.com/spec/v1
+oid sha256:bd5104722adf2fdab50bacecdb1ef7eff77fbe2e89294a18e760cf58c75c5ee5
+size 919
